ac6aa733b7bb9d5850ec2c43cc8c4aaa9d672ca3
vn70529 <than.ha@heb.com>
2018-03-24 15:19:34 +0700

    Add new feature

--- src/main/java/com/quangtk/DemoThymleaf/controller/MainController.java
+++ src/main/java/com/quangtk/DemoThymleaf/controller/MainController.java
@@ -7,6 +7,7 @@ import com.quangtk.DemoThymleaf.model.Person;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -61,6 +62,12 @@ public class MainController {
 		}
 		model.addAttribute("errorMessage", errorMessage);
 		return Constant.TEMPLATE_ADD_PERSON;
+	}
+
+	@GetMapping("/newFeature")
+	public String test(Model model) {
+		model.addAttribute("msg", "This is new feature");
+		return "newFeature";
 	}
 
 }
